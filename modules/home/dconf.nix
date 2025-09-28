f47feae104d93404bcd9bfc811a9767976573f68
steeren <steeren@gmail.com>
2025-09-28 16:29:39 +0200

    fixed wall paper issue

--- modules/home/dconf.nix
+++ modules/home/dconf.nix
@@ -6,6 +6,7 @@
       disable-user-extensions = false;
       enabled-extensions = with pkgs.gnomeExtensions; [
         dash-to-dock.extensionUuid
+        bing-wallpaper-changer.extensionUuid
       ];
     };
   };
