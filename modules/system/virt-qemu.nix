1427d53a2c46a4141a402c6bfc55b51292af860c
steeren <steeren@gmail.com>
2025-10-24 14:37:15 +0200

    libvert entry

--- modules/system/virt-qemu.nix
+++ modules/system/virt-qemu.nix
@@ -36,6 +36,7 @@
         # Use the full OVMF firmware (with network boot support)
         ovmf.packages = [ pkgs.OVMFFull.fd ];
       };
+
     };
     # Enable USB redirection in virt-manager
     spiceUSBRedirection.enable = true;
@@ -47,6 +48,19 @@
   #     uris = [ "qemu:///system" ];
   #   };
   # };
+
+  systemd.services.libvirt-default-network = {
+    description = "Start libvirt default network";
+    after = [ "libvirtd.service" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+      ExecStart = "${pkgs.libvirt}/bin/virsh net-start default";
+      ExecStop = "${pkgs.libvirt}/bin/virsh net-destroy default";
+      User = "root";
+    };
+  };
 
   programs.virt-manager.enable = true;
 }
